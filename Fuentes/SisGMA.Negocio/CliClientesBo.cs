2a92e9813a8bd572de96e857bbfcb916a8e7e842
Omar Carmona Rivas <ocarmona07@gmail.com>
2016-05-27 18:24:18 -0400

    Se actualiza en base al Entity Framework

---
+++ Fuentes/SisGMA.Negocio/CliClientesBo.cs
@@ -0,0 +1,20 @@
+﻿namespace SisGMA.Negocio
+{
+    using Entidades;
+    using Datos;
+
+    public class CliClientesBo
+    {
+        public CliClientes Get(int idItem)
+        {
+            return new CliClientesDa().Get(idItem);
+        }
+
+        public CliClientes Insert(CliClientes item)
+        {
+            return new CliClientesDa().Insert(item);
+        }
+
+
+    }
+}
---
+++ Fuentes/SisGMA.Vista/Index.aspx.cs
@@ -0,0 +1,36 @@
+﻿namespace SisGMA.Vista
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+    using Negocio;
+    using Entidades;
+
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (true)
+            {
+                var addCliente = new CliClientesBo().Insert(new CliClientes
+                {
+                    RutCliente = "159888207",
+                    Nombres = "Omar",
+                    ApPaterno = "Carmona",
+                    ApMaterno = "Rivas",
+                    Direccion = "Lynch 1040",
+                    Email = "ocarmona07@gmail.com",
+                    Estado = true,
+                    IdComuna = 211,
+                    Telefono = 82433600
+                });
+            }
+
+            var cliente = new CliClientesBo().Get(1);
+            lblCliente.Text = cliente.Nombres;
+        }
+    }
+}
